78adeae4eb6a3edc01b791ba89fa127f0ac981a7
Jonathan Dominguez <jonathan-g-dominguez@outlook.es>
2024-09-29 23:58:31 -0300

    Ejercicio 4 clase 4

---
+++ Clase4-Ejercicio4/src/ejercicio4/Main.java
@@ -0,0 +1,79 @@
+package ejercicio4;
+
+import stack.PilaEnlazada;
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PilaEnlazada<LinkedQueue> p1 = new PilaEnlazada<LinkedQueue>();
+		PilaEnlazada<LinkedQueue> p2 = new PilaEnlazada<LinkedQueue>();
+		
+		LinkedQueue<Integer> c1 = new LinkedQueue<Integer>();
+		LinkedQueue<Integer> c2 = new LinkedQueue<Integer>();
+		LinkedQueue<Integer> c3 = new LinkedQueue<Integer>();
+		
+		System.out.println("Cola 1");
+		cargarColas(c1);
+		System.out.println(c1);
+		System.out.println("Cola 2");
+		cargarColas(c2);
+		System.out.println(c2);
+		System.out.println("Cola 3");
+		cargarColas(c3);
+		System.out.println(c3);
+		p1.push(c1);
+		p1.push(c3);
+		p2.push(c2);
+		
+		System.out.println("El tamaño de la pila 1 es:"+p1.size());
+		System.out.println("El tamaño de la pila 2 es:"+p2.size());
+		
+		PilaEnlazada<LinkedQueue> pout= unirPilas(p1,p2);
+
+		
+		System.out.println(pout.top());
+		
+	}
+	
+	static void cargarColas(LinkedQueue cola) {
+		int cantidad = (int)(Math.random()*10);
+		
+		for(int i = 0; i<cantidad;i++) {
+			int numero = (int) (Math.random()*10);
+			System.out.print(numero+"-");
+			cola.enqueue(numero);
+		}
+		System.out.println();
+	}
+	
+	static PilaEnlazada<LinkedQueue> unirPilas(PilaEnlazada<LinkedQueue> p1, PilaEnlazada<LinkedQueue>p2){
+		PilaEnlazada<LinkedQueue> pout = new PilaEnlazada<LinkedQueue>();
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		
+		if(p1.size()>=p2.size()) {
+			while(p2.size()>0) {
+				pout.push(p2.pop());
+			}
+			}
+			else {
+				while(p1.size()>0) {
+					pout.push(p1.pop());
+				}
+			}
+		
+		if(p1.size()==0) {
+			pout.push(p2.pop());
+		}
+		else {
+			pout.push(p1.pop());
+		}
+		
+		return pout;
+	}
+
+}
---
+++ Clase4-Ejercicio4/src/stack/PilaEnlazada.java
@@ -0,0 +1,59 @@
+package stack;
+
+import ejercicio4.Node;
+ 
+
+public class PilaEnlazada<T> implements Stack<T> {
+	private Node<T> top;
+	private int size;
+	
+	public PilaEnlazada() {
+		top = null;
+		size = 0;
+	}
+	
+	public void push(T item) {
+		Node<T> nuevoNodo = new Node<T>(item,top);
+		top = nuevoNodo;
+		size++;
+	}
+	
+	public T pop() {
+		if(size == 0) {
+			System.out.println("La pila está vacia");
+			return null;
+		}
+		else {
+			Node<T> antiguoTop = top;
+			top = antiguoTop.getNext();
+			antiguoTop.setNext(null);
+			size--;
+			return antiguoTop.getElement();	
+		}
+	}
+	
+	public T top() {
+		if(size == 0) {
+			System.out.println("La pila está vacia");
+			return null;
+		}
+		return top.getElement();
+	}
+	
+	public boolean isEmpty() {
+		if (size == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+
+	
+}
+
